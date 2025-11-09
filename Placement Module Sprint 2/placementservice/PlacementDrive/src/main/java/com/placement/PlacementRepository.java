03159d7bb2c969847225792eb536535572d36e91
Abhinaya-TNSIF <159870800+Abhinaya-TNSIF@users.noreply.github.com>
2025-11-09 13:02:26 +0530

    Delete Placement Module Sprint 2/placementservice/PlacementDrive directory

--- Placement Module Sprint 2/placementservice/PlacementDrive/src/main/java/com/placement/PlacementRepository.java
+++
@@ -1,8 +0,0 @@
-package com.placement;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlacementRepository extends JpaRepository<Placement, Integer> {
-}
